6a7ed4562649ae05ddb1ca3b7ca5b1c212a4c1dd
Ubuntu <ubuntu@ip-172-31-24-29.ec2.internal>
2022-11-17 13:11:26 +0000

    first

---
+++ ansible/playbook/install_ansible.sh
@@ -0,0 +1,8 @@
+installAnsible () {
+  sudo apt-get update -y
+  sudo apt-get install software-properties-common -y
+  sudo apt-add-repository ppa:ansible/ansible -y
+  sudo apt-get update -y
+  sudo apt-get install ansible -y
+}
+installAnsible
